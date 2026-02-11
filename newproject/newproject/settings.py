7cab6c59f22a1b9ef71daa9ac4e907a20beef843
ButterChick <dhruvphooli@gmail.com>
2026-02-11 13:37:38 +0530

    Finally got it working

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'New_Users',
+        'HOST': r'localhost\SQLEXPRESS',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'trusted_connection': 'yes',
+        },
     }
 }
 
